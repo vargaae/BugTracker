b1b7d29e94ddaf7153fbb21c74ed139be2fadff1
vargaae <58937079+vargaae@users.noreply.github.com>
2021-10-10 23:19:23 +0200

    Add project files.

---
+++ BugTracker/Data/Migrations/20211010173656_initialSetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Data.Migrations
+{
+    public partial class initialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bug",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BugIssue = table.Column<string>(nullable: true),
+                    Project = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bug", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bug");
+        }
+    }
+}
---
+++ BugTracker/Models/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string BugIssue { get; set; }
+        public string Project { get; set; }
+        public Bug()
+        {
+
+        }
+    }
+}
